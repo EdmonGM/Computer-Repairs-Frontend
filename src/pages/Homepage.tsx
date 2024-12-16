977205286118b1fc401de7f2454b88b93c060029
Edmon <ed.moussa4@gmail.com>
2024-12-16 23:57:29 +0300

    added tickets table

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,23 @@
+import { useEffect } from "react";
 import useStore from "../app/store";
+import TicketsTable from "../components/homepage/TicketsTable";
 
 export function HomePage() {
-  const { username } = useStore();
+  const { username, decodeToken } = useStore();
+  useEffect(decodeToken, []);
 
   return (
     <>
-      <h1>Welcome {username}</h1>
-      <p></p>
+      <section className="my-4">
+        <h1>Welcome {username}!</h1>
+        <p>{new Date().toLocaleString().slice(0, -3)}</p>
+      </section>
+      <hr />
+      <section className="my-4">
+        <h3>Your Tickets</h3>
+        <TicketsTable />
+        <button className="btn btn-success">Create Ticket</button>
+      </section>
     </>
   );
 }
