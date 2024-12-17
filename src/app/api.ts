c834ecce0d505525df007a5b3958bb2d9773b815
Edmon <ed.moussa4@gmail.com>
2024-12-18 00:07:55 +0300

    added more api calls and extracted interfaces to another file

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import { ICreateTicket, ITicket, IUpdateTicket, IUser } from "./types";
 
 const api = axios.create({
   baseURL: "https://localhost:7198/api/",
@@ -37,14 +38,53 @@ async function LoginHandler(username: string, password: string) {
   return res;
 }
 
-async function GetCurrentUserTickets(): Promise<Array<Object>> {
+async function GetCurrentUserTickets(): Promise<Array<ITicket>> {
   let res = await api.get("/app-user/tickets");
   return res.data;
 }
 
-async function GetUserById(id: string): Promise<Object> {
+async function GetUserById(id: string): Promise<IUser> {
   let res = await api.get(`/app-user?userId=${id}`);
   return res.data;
 }
 
-export { LoginHandler, GetCurrentUserTickets, GetUserById };
+async function GetTicketById(id: string): Promise<ITicket> {
+  let res = await api.get(`/ticket/${id}`);
+  return res.data;
+}
+
+async function CreateTicket({
+  title,
+  description,
+  isCompleted,
+}: ICreateTicket) {
+  let res = await api.post("/ticket", {
+    title: title,
+    description: description,
+    isCompleted: isCompleted,
+  });
+  return res.data;
+}
+
+async function UpdateTicket({
+  id,
+  title,
+  description,
+  isCompleted,
+}: IUpdateTicket) {
+  let res = await api.put(`/ticket/${id}`, {
+    title: title,
+    description: description,
+    isCompleted: isCompleted,
+  });
+  return res.data;
+}
+
+export {
+  LoginHandler,
+  GetCurrentUserTickets,
+  GetUserById,
+  GetTicketById,
+  CreateTicket,
+  UpdateTicket,
+};
