c1dd005dccc68be6dd01f24d38628e4f0ddeaab3
Edmon <ed.moussa4@gmail.com>
2024-12-18 00:09:32 +0300

    added navigation

--- src/components/homepage/TicketsTableRow.tsx
+++ src/components/homepage/TicketsTableRow.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 interface Props {
-  id: string;
+  id: number;
   title: string;
   isCompleted: boolean;
 }
 function TicketsTableRow({ id, title, isCompleted }: Props) {
+  const navigate = useNavigate();
   return (
-    <tr className="cursor-pointer">
+    <tr className="cursor-pointer" onClick={() => navigate(`/tickets/${id}`)}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{isCompleted ? "True" : "False"}</td>
