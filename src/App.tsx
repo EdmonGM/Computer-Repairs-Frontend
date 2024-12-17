c849f94c6c486f57c15448dca93859998abe3f8c
Edmon <ed.moussa4@gmail.com>
2024-12-18 00:07:20 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { HomePage } from "./pages/Homepage";
 import Profile from "./pages/Profile";
 import { useEffect } from "react";
 import useStore from "./app/store";
+import TicketEdit from "./pages/TicketEdit";
+import TicketCreate from "./pages/TicketCreate";
 
 function App() {
   let url = useLocation().pathname;
@@ -19,6 +21,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/tickets">
+            <Route path=":id" element={<TicketEdit />} />
+            <Route path="new" element={<TicketCreate />} />
+          </Route>
         </Routes>
       </main>
     </>
