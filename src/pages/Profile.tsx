6f9550fba22bbb92b9bcc24011cb073fbbeecdb3
Edmon <ed.moussa4@gmail.com>
2024-12-18 00:10:34 +0300

    undefined safety

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -12,37 +12,39 @@ function Profile() {
 
   if (isFetching) return <p>Loading...</p>;
 
-  return (
-    <section className="my-4">
-      <h1>Your Profile</h1>
-      <div className="card profile-border">
-        <div className="row">
-          <div className="col-md-4 col d-flex justify-content-center">
-            <img
-              src="/public/person-1.png"
-              alt="Profile Picture"
-              className=""
-            />
-          </div>
-          <div className="col-md-8 col d-flex align-items-center">
-            <div className="card-body ">
-              <ul className="list-group list-group-flush">
-                <ProfileListItem title="Id" value={user?.id} />
-                <ProfileListItem title="Name" value={user?.name} />
-                <ProfileListItem title="Username" value={user?.userName} />
-                <ProfileListItem title="Email" value={user?.email} />
-                <ProfileListItem title="Salary" value={user?.salary} />
-                <ProfileListItem
-                  title="Number of tickets"
-                  value={user?.tickets.length}
-                />
-              </ul>
+  if (user) {
+    return (
+      <section className="my-4">
+        <h1>Your Profile</h1>
+        <div className="card profile-border">
+          <div className="row">
+            <div className="col-md-4 col d-flex justify-content-center">
+              <img
+                src="/public/person-1.png"
+                alt="Profile Picture"
+                className=""
+              />
+            </div>
+            <div className="col-md-8 col d-flex align-items-center">
+              <div className="card-body ">
+                <ul className="list-group list-group-flush">
+                  <ProfileListItem title="Id" value={user.id} />
+                  <ProfileListItem title="Name" value={user.name} />
+                  <ProfileListItem title="Username" value={user.userName} />
+                  <ProfileListItem title="Email" value={user.email} />
+                  <ProfileListItem title="Salary" value={user.salary} />
+                  <ProfileListItem
+                    title="Number of tickets"
+                    value={user.tickets.length}
+                  />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default Profile;
