166a5d016e625c43b9ba83ccf5ff6282d3dcde22
Edmon <ed.moussa4@gmail.com>
2024-12-19 01:37:52 +0300

    removed tokenDecode

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,30 +1,15 @@
 import { create } from "zustand";
-import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
 
 interface UserState {
   username: string;
   email: string;
   id: string;
-  decodeToken: () => void;
 }
-type TokenPayload = {
-  id: string;
-  username: string;
-  email: string;
-};
 
 const useStore = create<UserState>((set) => ({
   username: "",
   email: "",
   id: "",
-  decodeToken: () => {
-    let token = Cookies.get("access");
-    if (token) {
-      let { id, username, email }: TokenPayload = jwtDecode(token);
-      set({ id: id, username: username, email: email });
-    }
-  },
 }));
 
 export default useStore;
