e6f67911c060c59e7bb85d6742563d2bee9a92a8
Edmon <ed.moussa4@gmail.com>
2024-12-19 23:35:21 +0300

    added setData method

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,12 +4,16 @@ interface UserState {
   username: string;
   email: string;
   id: string;
+  setData: (username: string, email: string, id: string) => void;
 }
 
 const useStore = create<UserState>((set) => ({
   username: "",
   email: "",
   id: "",
+  setData: (username, email, id) => {
+    set({ username: username, email: email, id: id });
+  },
 }));
 
 export default useStore;
