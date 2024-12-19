e9bfa2ad04919b86010459ddb023e22131685f2f
Edmon <ed.moussa4@gmail.com>
2024-12-19 23:31:25 +0300

    removed token decode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Navbar from "./pages/Navbar";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Homepage";
 import Profile from "./pages/Profile";
-import { useEffect } from "react";
-import useStore from "./app/store";
 import TicketEdit from "./pages/TicketEdit";
 import TicketCreate from "./pages/TicketCreate";
 
 function App() {
-  let url = useLocation().pathname;
-  const decodeToken = useStore((state) => state.decodeToken);
-  useEffect(decodeToken, [url]);
   return (
     <>
       <Navbar />
