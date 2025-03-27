fd54875bfeefeff9ddaca85723e5134f343fcd7c
Edmon <ed.moussa4@gmail.com>
2025-03-27 22:18:31 +0300

    Added changing password feature

--- src/components/profile/ProfileEditModal.tsx
+++ src/components/profile/ProfileEditModal.tsx
@@ -1,4 +1,6 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { IUpdateUser } from "../../app/types";
+import { UpdateCurrentUser } from "../../app/api";
 
 interface Props {
   id: string;
@@ -12,15 +14,23 @@ interface Props {
 interface Inputs {
   email: string;
   name: string;
-  password: string;
+  changingPassword: boolean;
+  currentPassword: string;
+  newPassword: string;
 }
 function ProfileEditModal({ id, label, title, user }: Props) {
   const {
     register,
     handleSubmit,
+    getValues,
+    watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data: IUpdateUser) =>
+    UpdateCurrentUser({ ...data });
+
+  watch("changingPassword");
+
   return (
     <div
       className="modal fade"
@@ -43,55 +53,92 @@ function ProfileEditModal({ id, label, title, user }: Props) {
             ></button>
           </div>
           <div className="modal-body">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="mb-3">
-                <label htmlFor="EmailInput" className="form-label">
-                  Email Address
-                </label>
-                <input
-                  id="EmailInput"
-                  placeholder="name@example.com"
-                  className="form-control"
-                  type="email"
-                  defaultValue={user.email}
-                  {...register("email", { required: true })}
-                />
-                {errors.email && (
-                  <span className="text-danger">This field is required!</span>
-                )}
+            <div className="mb-3">
+              <label htmlFor="EmailInput" className="form-label">
+                Email Address
+              </label>
+              <input
+                id="EmailInput"
+                placeholder="name@example.com"
+                className="form-control"
+                type="email"
+                defaultValue={user.email}
+                {...register("email", { required: true })}
+              />
+              {errors.email && (
+                <span className="text-danger">This field is required!</span>
+              )}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="NameInput" className="form-label">
+                Name
+              </label>
+              <input
+                id="NameInput"
+                placeholder="Your Name"
+                className="form-control"
+                defaultValue={user.name}
+                {...register("name", { required: true })}
+              />
+              {errors.name && (
+                <span className="text-danger">This field is required!</span>
+              )}
+            </div>
+            <div className="mb-3 form-check form-switch">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckDefault"
+                {...register("changingPassword")}
+              />
+              <label
+                className="form-check-label ms-1"
+                htmlFor="flexSwitchCheckDefault"
+              >
+                Change Password?
+              </label>
+            </div>
+            {getValues("changingPassword") && (
+              <div>
+                <div className="mb-3">
+                  <label htmlFor="CurrentPasswordInput" className="form-label">
+                    Current Password
+                  </label>
+                  <input
+                    id="CurrentPasswordInput"
+                    className="form-control"
+                    type="password"
+                    {...register("currentPassword", {
+                      required: getValues("changingPassword"),
+                    })}
+                  />
+                  {errors.currentPassword && (
+                    <span className="text-danger">
+                      Please enter current password!
+                    </span>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="NewPasswordInput" className="form-label">
+                    New Password
+                  </label>
+                  <input
+                    id="NewPasswordInput"
+                    className="form-control"
+                    type="password"
+                    {...register("newPassword", {
+                      required: getValues("changingPassword"),
+                    })}
+                  />
+                  {errors.newPassword && (
+                    <span className="text-danger">
+                      Please enter new password!
+                    </span>
+                  )}
+                </div>
               </div>
-              <div className="mb-3">
-                <label htmlFor="NameInput" className="form-label">
-                  Name
-                </label>
-                <input
-                  id="NameInput"
-                  placeholder="Your Name"
-                  className="form-control"
-                  defaultValue={user.name}
-                  {...register("name", { required: true })}
-                />
-                {errors.name && (
-                  <span className="text-danger">This field is required!</span>
-                )}
-              </div>
-              <div className="mb-3">
-                <label htmlFor="PasswordInput" className="form-label">
-                  Password
-                </label>
-                <input
-                  id="PasswordInput"
-                  className="form-control"
-                  type="password"
-                  {...register("password", { required: true })}
-                />
-                {errors.password && (
-                  <span className="text-danger">
-                    Password is required to make changes!
-                  </span>
-                )}
-              </div>
-            </form>
+            )}
           </div>
           <div className="modal-footer">
             <button
@@ -101,9 +148,7 @@ function ProfileEditModal({ id, label, title, user }: Props) {
             >
               Close
             </button>
-            <button type="submit" className="btn btn-success">
-              Save changes
-            </button>
+            <input type="submit" className="btn btn-success" />
           </div>
         </form>
       </div>
