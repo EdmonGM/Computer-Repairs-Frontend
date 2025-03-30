908c8c55c38bd93e39611c78380603be96956f4e
Edmon <ed.moussa4@gmail.com>
2025-03-30 13:05:34 +0300

    moved profileEditModal to App component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,7 +3,6 @@ import { GetUserById } from "../app/api";
 import useStore from "../app/store";
 import ProfileListItem from "../components/profile/ProfileListItem";
 import ProfileEditModalBtn from "../components/profile/ProfileEditModalBtn";
-import ProfileEditModal from "../components/profile/ProfileEditModal";
 
 function Profile() {
   const id = useStore((state) => state.id);
@@ -16,44 +15,36 @@ function Profile() {
 
   if (user) {
     return (
-      <section className="my-4">
-        <h1>Your Profile</h1>
-        <main className="card profile-border">
-          <div className="row">
-            <div className="col-md-4 col d-flex justify-content-center">
-              <img
-                src="/public/person-1.png"
-                alt="Profile Picture"
-                className=""
-              />
-            </div>
-            <div className="col-md-8 col d-flex align-items-center">
-              <div className="card-body ">
-                <ul className="list-group list-group-flush">
-                  <ProfileListItem title="Id" value={user.id} />
-                  <ProfileListItem title="Name" value={user.name} />
-                  <ProfileListItem title="Username" value={user.userName} />
-                  <ProfileListItem title="Email" value={user.email} />
-                  <ProfileListItem title="Salary" value={user.salary + "$"} />
-                  <ProfileListItem
-                    title="Number of tickets"
-                    value={user.tickets.length}
-                  />
-                </ul>
+      <>
+        <section className="my-4">
+          <h1>Your Profile</h1>
+          <main className="card profile-border">
+            <div className="row">
+              <div className="col-md-4 col d-flex justify-content-center">
+                <img src="/person-1.png" alt="Profile Picture" className="" />
+              </div>
+              <div className="col-md-8 col d-flex align-items-center">
+                <div className="card-body ">
+                  <ul className="list-group list-group-flush">
+                    <ProfileListItem title="Id" value={user.id} />
+                    <ProfileListItem title="Name" value={user.name} />
+                    <ProfileListItem title="Username" value={user.userName} />
+                    <ProfileListItem title="Email" value={user.email} />
+                    <ProfileListItem title="Salary" value={user.salary + "$"} />
+                    <ProfileListItem
+                      title="Number of tickets"
+                      value={user.tickets.length}
+                    />
+                  </ul>
+                </div>
               </div>
             </div>
+          </main>
+          <div className="my-4">
+            <ProfileEditModalBtn id="ProfileEdit" body="Edit Profile" />
           </div>
-        </main>
-        <div className="my-4">
-          <ProfileEditModalBtn id="ProfileEdit" body="Edit Profile" />
-        </div>
-        <ProfileEditModal
-          id="ProfileEdit"
-          label="ProfileEditModal"
-          title="Edit Your Profile"
-          user={user}
-        />
-      </section>
+        </section>
+      </>
     );
   }
 }
