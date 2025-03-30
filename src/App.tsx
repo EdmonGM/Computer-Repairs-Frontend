3c7a3143a0312959aec06ead34e9721eccd1b371
Edmon <ed.moussa4@gmail.com>
2025-03-30 13:03:04 +0300

    moved currentUser api call here

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,38 @@ import Profile from "./pages/Profile";
 import TicketEdit from "./pages/TicketEdit";
 import TicketCreate from "./pages/TicketCreate";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { GetCurrentUser } from "./app/api";
+import useStore from "./app/store";
+import ProfileEditModal from "./components/profile/ProfileEditModal";
 
 function App() {
+  const { setData, username } = useStore();
+  const {
+    data: user,
+    isSuccess,
+    isFetching,
+  } = useQuery({
+    queryFn: GetCurrentUser,
+    queryKey: ["users"],
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    staleTime: Infinity,
+  });
+  useEffect(() => {
+    if (isSuccess) setData(user.userName, user.email, user.id, user.name);
+  }, [isSuccess]);
   return (
     <>
       <Navbar />
       <main className="container">
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/"
+            element={<HomePage username={username} isFetching={isFetching} />}
+          />
           <Route path="/profile" element={<Profile />} />
           <Route path="/tickets">
             <Route path=":id" element={<TicketEdit />} />
@@ -24,6 +47,12 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
+
+      <ProfileEditModal
+        id="ProfileEdit"
+        label="ProfileEditModal"
+        title="Edit Your Profile"
+      />
     </>
   );
 }
