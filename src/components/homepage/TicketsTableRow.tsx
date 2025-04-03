4619a0bb06e0528231889177cdf8b0a2da4a5ec4
Edmon <ed.moussa4@gmail.com>
2025-04-03 21:08:46 +0300

    Added the ability to add more rows to the component

--- src/components/homepage/TicketsTableRow.tsx
+++ src/components/homepage/TicketsTableRow.tsx
@@ -1,17 +1,20 @@
+import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface Props {
   id: number;
   title: string;
   isCompleted: boolean;
+  children?: ReactNode;
 }
-function TicketsTableRow({ id, title, isCompleted }: Props) {
+function TicketsTableRow({ id, title, isCompleted, children }: Props) {
   const navigate = useNavigate();
   return (
     <tr className="cursor-pointer" onClick={() => navigate(`/tickets/${id}`)}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{isCompleted ? "True" : "False"}</td>
+      {children}
     </tr>
   );
 }
