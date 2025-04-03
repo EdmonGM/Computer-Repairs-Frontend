d2571c9df26804351b39970e5d7cc3f9ab8270d5
Edmon <ed.moussa4@gmail.com>
2025-04-03 17:38:49 +0300

    Used store to access username

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,14 @@
+import { useUserStore } from "../app/store";
 import TicketsTable from "../components/homepage/TicketsTable";
 import { Link } from "react-router-dom";
 
-interface Props {
-  username: string;
-  isFetching: boolean;
-}
-export function HomePage({ username, isFetching }: Props) {
+export function HomePage() {
+  const { username } = useUserStore();
+
   return (
     <>
       <section className="my-4">
-        {isFetching ? <h1>Welcome</h1> : <h1>Welcome {username}!</h1>}
+        <h1>Welcome {username}!</h1>
         <p>{new Date().toLocaleString().slice(0, -3)}</p>
       </section>
       <hr />
