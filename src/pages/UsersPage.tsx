0591569b80999d56f8c491074948fac888d08c31
Edmon <ed.moussa4@gmail.com>
2025-04-04 16:28:10 +0300

    Added link to CreateUserPage component

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import UsersTable from "../components/usersPage/UsersTable";
 
 function UsersPage() {
@@ -8,6 +9,9 @@ function UsersPage() {
       </section>
       <section>
         <UsersTable />
+        <Link to="new" className="btn btn-success">
+          Create User
+        </Link>
       </section>
     </>
   );
